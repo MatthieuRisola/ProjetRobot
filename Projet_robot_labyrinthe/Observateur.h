1c9f40053da2b3808ce75eaefe688d51f28a98fc
MatthieuRisola <matthieurisola@hotmail.com>
2024-12-08 19:05:20 +0100

    Update Observateur.h
    ajout observateur derniere position pour laffichage

--- Projet_robot_labyrinthe/Observateur.h
+++ Projet_robot_labyrinthe/Observateur.h
@@ -1,18 +1,20 @@
 #ifndef OBSERVATEUR_H
 #define OBSERVATEUR_H
 
-
 class Observateur {
 public:
     virtual ~Observateur() = default;
     virtual void update(int x, int y, int direction) = 0;
 };
 
+/*
 class ObservateurAffichage : public Observateur {
 public:
     void update(int x, int y, int direction) override;
 };
+*/
 
+//observateur pour lalgorithme principal
 class ObservateurComptageDeplacements : public Observateur {
 public:
     ObservateurComptageDeplacements();
@@ -22,14 +24,23 @@ private:
     int d_nombreDeplacements;
 };
 
+//observateur pour lalgorithme principal
 class ObservateurComptageDirections : public Observateur {
 public:
 	ObservateurComptageDirections();
-void update(int x, int y, int direction) override;
+    void update(int x, int y, int direction) override;
     int nombreDirections() const;
 private:
     int d_nombreDirections;
 };
 
-
-#endif // OBSERVATEUR_H
+//observateur necessaire pour mettre a jour laffichage
+class ObservateurDernierePosition : public Observateur {
+public:
+    ObservateurDernierePosition();
+    void update(int x, int y);
+    int dernierX();
+    int dernierY();
+private:
+    int d_x, d_y;
+};
