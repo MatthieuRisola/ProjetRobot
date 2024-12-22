6a2b2efca143de304ee5dbec822fa7b1498a86dc
MatthieuRisola <matthieurisola@hotmail.com>
2024-12-22 14:48:55 +0100

    Update Observateur.cpp
    modifications en fonctions des changements sur le .h

--- Projet_robot_labyrinthe/Observateur.cpp
+++ Projet_robot_labyrinthe/Observateur.cpp
@@ -1,11 +1,29 @@
 #include "Observateur.h"
+#include "Labyrinthe.h"
+#include "Affichage.h"
+#include <iostream>
 
-ObservateurComptageDeplacements::ObservateurComptageDeplacements() : d_nombreDeplacements{0}
+ObservateurAffichage::ObservateurAffichage(const Affichage &affichage, const Labyrinthe &labyrinthe, int x, int y, int direction) : d_affichage{affichage}, d_labyrinthe{labyrinthe}, d_ancienX{x}, d_ancienY{y}, d_ancienneDirection{direction}
+{}
+
+void ObservateurAffichage::update(int x, int y, int direction)
+{
+    d_affichage.update(d_labyrinthe, d_ancienX, d_ancienY, x, y, direction);
+    d_ancienX=x;
+    d_ancienY=y;
+    d_ancienneDirection=direction;
+}
+
+ObservateurComptageDeplacements::ObservateurComptageDeplacements(int x, int y) : d_nombreDeplacements{0}, d_ancienX{x}, d_ancienY{y}
 {}
 
 void ObservateurComptageDeplacements::update(int x, int y, int direction)
 {
-    ++d_nombreDeplacements;
+    if(d_ancienX!=x||d_ancienY!=y){
+        ++d_nombreDeplacements;
+        d_ancienX=x;
+        d_ancienY=y;
+    }
 }
 
 int ObservateurComptageDeplacements::nombreDeplacements() const
@@ -13,12 +31,15 @@ int ObservateurComptageDeplacements::nombreDeplacements() const
     return d_nombreDeplacements;
 }
 
-ObservateurComptageDirections::ObservateurComptageDirections() : d_nombreDirections{0}
+ObservateurComptageDirections::ObservateurComptageDirections(int direction) : d_nombreDirections{0}, d_ancienneDirection{direction}
 {}
 
 void ObservateurComptageDirections::update(int x, int y, int direction)
 {
-    ++d_nombreDirections;
+        if(d_ancienneDirection!=direction){
+            ++d_nombreDirections;
+            d_ancienneDirection=direction;
+    }
 }
 
 int ObservateurComptageDirections::nombreDirections() const
