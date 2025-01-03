103c0fa4c8f3005ed969412c2c8c2abc80d46362
MatthieuRisola <matthieurisola@hotmail.com>
2025-01-03 22:32:41 +0100

    Update main.cpp
    ajout structure des methodes principales, test de fonctionnement global des classes dans le main

--- Projet_robot_labyrinthe/main.cpp
+++ Projet_robot_labyrinthe/main.cpp
@@ -5,15 +5,65 @@
 
 using namespace std;
 
+Labyrinthe selectionLabyrinthe(const vector<string> &nomFichiersLabyrinthes, const Affichage &aff)
+{
+    int i=0;
+    for(const auto &fichier : nomFichiersLabyrinthes)
+    {
+        Labyrinthe laby{};
+        laby.lisDepuisFichier(fichier);
+        cout<<"Labyrinthe "<<i<<" :"<<endl;
+        laby.afficheSansRobot(aff);
+        cout<<endl<<endl;
+        ++i;
+    }
+}
+
+void selectionAlgorithme()
+{
+
+}
+
+void mainDroite(Robot &rob, const Labyrinthe &laby)
+{
+
+}
+
+void pledge(Robot &rob, const Labyrinthe &laby)
+{
+
+}
+
 int main()
 {
-    Robot r(14,5,Direction::HAUT);
-    AffichageTexteSimple aff{};
+    Robot r(5,6,Direction::HAUT);
+    AffichageTexteAmeliore1 aff{};
     Labyrinthe laby{};
-    laby.lisDepuisFichier("test.txt");
+    laby.lisDepuisFichier("testaffichage.txt");
     aff.afficheDepart(laby, r);
     ObservateurAffichage obs(aff,laby,r.x(),r.y(),r.direction());
     r.ajouteObservateur(std::make_unique<ObservateurAffichage>(obs));
+    getch();
+    r.avance();
+    r.notifieObservateurs();
+    getch();
+    r.tourneDroite();
+    r.notifieObservateurs();
+    getch();
+    r.avance();
+    r.notifieObservateurs();
+    getch();
+    r.avance();
+    r.notifieObservateurs();
+    getch();
+    r.avance();
+    r.notifieObservateurs();
+    getch();
+    r.avance();
+    r.notifieObservateurs();
+    getch();
+    r.avance();
+    r.notifieObservateurs();
     getch();
     r.avance();
     r.notifieObservateurs();
