ae23467cf40cd33b2d2997a466152e51a313807f
Tieu Han <126774336+Pimbolie@users.noreply.github.com>
2025-01-04 16:50:47 +0100

    Update Robot.h
    Suppression des remarques en commentaire

--- Projet_robot_labyrinthe/Robot.h
+++ Projet_robot_labyrinthe/Robot.h
@@ -5,6 +5,8 @@
 #include <memory>
 #include "Observateur.h"
 
+
+
 class Labyrinthe;
 class Affichage;
 
@@ -34,25 +36,5 @@ private:
     int d_x, d_y, d_direction;
     std::vector<std::unique_ptr<Observateur>> d_observateurs;
 };
-
-/** REMARQUE 1 :
-j'ai ajoute enum Direction HAUT, DROITE, BAS, GAUCHE **/
-
-/** REMARQUE 2 :
-pour Robot::afficher, je propose une methode Affichage::afficherRobot(const Robot& r) de sorte que :
-    -   dans Robot::afficher : on fait juste aff.afficheRobot(*this);
-    -   dans Affichage::afficherRobot(const Robot& r) : on fait ce qu'il faut pour mettre à jour l'ancienne case où était le robot
-        et la nouvelle case où il y a le robot **/
-
-/** REMARQUE 3 :
-je n'ai pas fait enleverObservateur car je n'ai pas l'impression que ça va servir **/
-
-/** 08/12/2024 ______________________________________________________________________________________________________________________________**/
-
-/** REMARQUE 4 : changeDirection() pour pouvoir mieux factoriser les tests ? **/
-
-/** REMARQUE 5 : debut de chaque classe en Majuscule **/
-
-/** REMARQUE 6 : avancer -> avance, notifier -> notifie, tournerGauche->tourneGauche, etc.. **/
 
 #endif // ROBOT_H
