bf71f48d0dcd7270767f1ef07387e7aa0638b34f
Kaithlyn04 <miesch.kaithlyn@outlook.fr>
2025-01-05 21:08:57 +0100

    Ajout du fichier DeplacementRobottest.cpp qui teste la classe DeplacementRobot

---
+++ Projet_robot_labyrinthe/DeplacementRobottest.cpp
@@ -0,0 +1,19 @@
+#define DOCTEST_CONFIG_NO_MULTITHREADING
+#include "doctest.h"
+#include "DeplacementRobot.h"
+#include "Robot.h"
+
+/*TEST_CASE ("Test des déplacements de Robot")
+{
+    int x=3;
+    int y=5;
+    int direction = HAUT;
+    Robot r{x,y, direction};
+
+    SUBCASE("Le robot avance correctement")
+    {
+        RobotAvance Avance;
+        Avance.manipulate(r);
+
+    }
+}*/
