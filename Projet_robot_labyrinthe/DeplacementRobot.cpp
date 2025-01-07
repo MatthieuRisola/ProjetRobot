b3609dc0d77cec4483669e0b1f1a76b65bc39202
MatthieuRisola <matthieurisola@hotmail.com>
2025-01-07 19:52:03 +0100

    Update DeplacementRobot.cpp
    ajout delai a chaque deplacement pour que le deplacement soit automatique

--- Projet_robot_labyrinthe/DeplacementRobot.cpp
+++ Projet_robot_labyrinthe/DeplacementRobot.cpp
@@ -1,19 +1,24 @@
 #include "DeplacementRobot.h"
 #include "Robot.h"
+#include <thread>
+#include <chrono>
 
 void RobotAvance::manipulate(Robot &r) const
 {
     r.avance();
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
 }
 
 
 void RobotGauche::manipulate(Robot &r) const
 {
     r.tourneGauche();
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
 }
 
 
 void RobotDroit::manipulate(Robot &r) const
 {
     r.tourneDroite();
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
 }
